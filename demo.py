c2e9bbe1478b6fe056a57ee965344029c3351e2a
Jarosław Bieniek <jaroslaw.bieniek255@outlook.com>
2019-10-22 18:00:17 +0200

    Add demo first code

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod, ABC
+
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import TextOperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import TextRecognitionMode
@@ -8,8 +10,40 @@ import os
 import sys
 import time
 
-endpoint = "https://mcvdemo.cognitiveservices.azure.com/"
-with open(".apipass", "r") as apiPassFile:
-    subscription_key = apiPassFile.readline()
+
+class MCVModelUsage(ABC):
+    def __init__(self, endpoint_url, subscription_key, feature):
+        self._endpoint_url = endpoint_url
+        self._subscription_key = subscription_key
+        self._feature = feature
+        self._final_output = None
+
+    def run_analyze(self):
+        pass
+
+    def get_final_output(self):
+        return self._final_output
+
+    @abstractmethod
+    def do_work(self):
+        pass
+
+
+class MCVModelUsageConcreteImpl(MCVModelUsage):
+    pass
+
+
+class MCVImageDescriptor(MCVModelUsage):
+    pass
+
+
+cv_model_usages = [
+    #
+]
+
+if __name__ == "__main__":
+    endpoint = "https://mcvdemo.cognitiveservices.azure.com/"
+    with open(".apipass", "r") as apiPassFile:
+        subscription_key = apiPassFile.readline()
 
 
